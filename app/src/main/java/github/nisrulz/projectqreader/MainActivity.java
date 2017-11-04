a1f218eef128ea9a7d76489812cc8a0289476917
Nishant Srivastava <nisrulz@gmail.com>
2017-11-04 00:48:28 -0700

    updated sample app

--- app/src/main/java/github/nisrulz/projectqreader/MainActivity.java
+++ app/src/main/java/github/nisrulz/projectqreader/MainActivity.java
@@ -23,15 +23,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import github.nisrulz.qreader.QRDataListener;
 import github.nisrulz.qreader.QREader;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Menu menu;
 
     private static final String cameraPerm = Manifest.permission.CAMERA;
 
@@ -53,40 +56,15 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.code_info);
 
-        final Button stateBtn = findViewById(R.id.btn_start_stop);
-        // change of reader state in dynamic
-        stateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (qrEader.isCameraRunning()) {
-                    stateBtn.setText("Start QREader");
-                    qrEader.stop();
-                } else {
-                    stateBtn.setText("Stop QREader");
-                    qrEader.start();
-                }
-            }
-        });
-
-        stateBtn.setVisibility(View.VISIBLE);
-
-        Button restartbtn = findViewById(R.id.btn_restart_activity);
-        restartbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                restartActivity();
-            }
-        });
-
         // Setup SurfaceView
         // -----------------
         mySurfaceView = findViewById(R.id.camera_view);
 
         if (hasCameraPermission) {
             // Setup QREader
-            // setupQREader();
-            readQRCodeFromDrawable(R.drawable.img_qrcode);
+            setupQREader();
 
+            //readQRCodeFromDrawable(R.drawable.img_qrcode);
 
         } else {
             RuntimePermissionUtil.requestPermission(MainActivity.this, cameraPerm, 100);
@@ -118,6 +96,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_start_pause_preview:
+                if (qrEader.isCameraRunning()) {
+                    qrEader.stop();
+                    menu.findItem(item.getItemId()).setIcon(R.drawable.img_start);
+                } else {
+                    qrEader.start();
+                    menu.findItem(item.getItemId()).setIcon(R.drawable.img_pause);
+                }
+                break;
+
+            case R.id.menu_restart:
+                restartActivity();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull final String[] permissions,
             @NonNull final int[] grantResults) {
         if (requestCode == 100) {
@@ -135,11 +142,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-    }
-
-    void restartActivity() {
-        startActivity(new Intent(MainActivity.this, MainActivity.class));
-        finish();
     }
 
     void readQRCodeFromDrawable(int resID) {
@@ -191,5 +193,10 @@ public class MainActivity extends AppCompatActivity {
                 .width(mySurfaceView.getWidth())
                 .surfaceView(mySurfaceView)
                 .build();
+    }
+
+    private void restartActivity() {
+        startActivity(new Intent(MainActivity.this, MainActivity.class));
+        finish();
     }
 }
